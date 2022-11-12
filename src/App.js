7e7ac497a45df5ae2c562dd88980034f7ae64d16
asami <super.pylori6@gmail.com>
2022-11-12 17:52:57 +0900

    mmm

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ import routes from "routes";
 import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 // Images
-import brandWhite from "assets/images/logo-ct5.png";
+import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
 
 export default function App() {
